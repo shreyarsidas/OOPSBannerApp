5f18e8e02cd56b286a59bc60bd3e45c28e8eacee
shreyarsidas <shreyarsidas@gmail.com>
2026-02-23 19:42:12 +0530

    neww

---
+++ src/UC2.java
@@ -0,0 +1,14 @@
+public class UC2{
+	public static void main(){
+		System.out.println("   ***   " + "   ***   " + " ******  " + "   ****  ");
+		System.out.println(" **   ** " + " **   ** " + " **    * " + "  *      ");
+		System.out.println("*       *" + "*       *" + " **    * " + "  *      ");
+		System.out.println("*       *" + "*       *" + " **   ** " + "   ***   ");
+		System.out.println("*       *" + "*       *" + " *****   " + "      *  ");
+		System.out.println("*       *" + "*       *" + " **      " + "      *  ");
+		System.out.println(" **   ** " + " **   ** " + " **      " + "      *  ");
+		System.out.println("   ***   " + "   ***   " + " **      " + "   ***   ");
+		
+	}
+		
+}
